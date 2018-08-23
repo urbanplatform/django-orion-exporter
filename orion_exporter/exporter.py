7444e87b13694ffe598069bee090b1cce6e5300d
Francisco Torres Cardoso <fcardoso@ubiwhere.com>
2018-08-23 12:32:33 +0100

    Bug correction

--- orion_exporter/exporter.py
+++ orion_exporter/exporter.py
@@ -38,18 +38,18 @@ def send_request(body, headers):
         "Content-Type": "application/json"
     }
 
-    # try:
-    #     orion_request = requests.post("{}v2/op/update".format(ORION_URL), data=json.dumps(body), headers=headers)
-    #     print(orion_request, orion_request.text)
-    # except:
-    #     logging.exception("Failed to send update to orion for entity {} with Fiware Headers".format(body))
-    #
-    # try:
-    #     orion_request = requests.post("{}v2/op/update".format(ORION_URL), data=json.dumps(body), headers=clean_headers)
-    #     print(orion_request, orion_request.text)
-    # except:
-    #     logging.exception("Failed to send update to orion for entity {} without Fiware Headers".format(body))
-    #
+    try:
+        orion_request = requests.post("{}v2/op/update".format(ORION_URL), data=json.dumps(body), headers=headers)
+        print(orion_request, orion_request.text)
+    except:
+        logging.exception("Failed to send update to orion for entity {} with Fiware Headers".format(body))
+
+    try:
+        orion_request = requests.post("{}v2/op/update".format(ORION_URL), data=json.dumps(body), headers=clean_headers)
+        print(orion_request, orion_request.text)
+    except:
+        logging.exception("Failed to send update to orion for entity {} without Fiware Headers".format(body))
+
 
 
 def remove_bad_chars(value):
