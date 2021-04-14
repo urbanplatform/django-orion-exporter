3df5bbda88ed983571bb299fc6dcdb252ba73d1d
Francisco Cardoso <fcardoso@ubiwhere.com>
2021-04-14 10:54:02 +0100

    [TMP] removed gevent and used normal request

--- orion_exporter/management/commands/send_to_orion.py
+++ orion_exporter/management/commands/send_to_orion.py
@@ -1,9 +1,9 @@
-from __future__ import print_function
-
-import gevent
-from gevent import monkey
-
-monkey.patch_all(thread=False)
+# from __future__ import print_function
+#
+# import gevent
+# from gevent import monkey
+#
+# monkey.patch_all(thread=False)
 
 from django.core.management.base import BaseCommand
 
@@ -21,16 +21,17 @@ class Command(BaseCommand):
         obj.send_to_orion(obj)
 
     def parse_models(self, models):
-        tasks = []
+        # tasks = []
         for model in models:
             if model._meta.abstract:  # If model is abstract
                 self.parse_models(model.__subclasses__())
             else:  # If model is not abstract
                 objects = model.objects.filter(sent_to_orion=False)
                 for obj in objects:
-                    tasks.append(gevent.spawn(self.send_request, obj))
+                    self.send_request(obj)
+                    # tasks.append(gevent.spawn(self.send_request, obj))
 
-        gevent.wait(tasks)
+        # gevent.wait(tasks)
 
     def handle(self, *args, **options):
         # Get inherited model from Orion exporter model and get their objects
