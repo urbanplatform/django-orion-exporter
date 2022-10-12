ea2f03cc64961af7c5aaf744b66a2ffddb744de0
Diogo Silva <diogosilv30@gmail.com>
2022-10-12 18:33:21 +0100

    Added Missing Requirements

--- setup.py
+++ setup.py
@@ -2,38 +2,37 @@ import os
 from setuptools import find_packages
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='orion_exporter',
-    version='0.0.1',
+    name="orion_exporter",
+    version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'Django>=2.2',
-    ],    
+    install_requires=open("requirements.txt").readlines(),
     zip_safe=False,
-    license='MIT',
-    description='A simple Django app for exporting models to Orion',
-    long_description=README,
-    url='https://gitlab.ubiwhere.com/mbaas/apps/orion_exporter.git',
-    author='Francisco Monsanto',
-    author_email='fmonsanto@ubiwhere.com',
+    license="MIT",
+    description="A simple Django app for exporting models to Orion",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.ubiwhere.com/mbaas/apps/orion_exporter.git",
+    author="Francisco Monsanto",
+    author_email="fmonsanto@ubiwhere.com",
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ]
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
 )
-
